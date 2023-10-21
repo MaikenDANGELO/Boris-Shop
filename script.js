5b6ad8b7d0b6e7aee1590191e5c40d61f8a0ec63
Mehdi BEN SMAIL <mehdi.ben_smail@edu.univ-fcomte.fr>
2023-10-21 11:09:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 const main = document.querySelector('.main');
 const sortSelect = document.getElementById('sort-by');
 const showMoreButton = document.getElementById('show-more');
@@ -7,25 +9,26 @@ const additionalProducts = [
         image: '/images/produit/phage.webp',
         title: 'Phage',
         description: 'Pour enfoncer les clous les plus coriaces',
+        dataCategory: 'category2',
         category: 'Divers',
         color: 'Blanc',
-        price: '109,99 €',
-    },
-    {
+        price: '109,99 €'
+    }, {
         image: '/images/produit/sorcerers-shoes.png',
         title: 'Air Sorcerer 1',
         description: 'Pour se déplacer en toute élégance',
-        category: 'Vêtement',
+        dataCategory: 'category1',
+        category: 'Vetement',
         color: 'Bleu',
-        price: '109,99 €',
-    },
-    {
+        price: '109,99 €'
+    }, {
         image: '/images/produit/negatrons-cloak.png',
         title: 'Manteau de Négatron',
         description: 'Cette cape permet de vous donne des stats de RM (Résistance au ManqueDeFlow)',
-        category: 'Vêtement',
+        dataCategory: 'category1',
+        category: 'Vetement',
         color: 'Blanc',
-        price: '89,99 €',
+        price: '89,99 €'
     },
 ];
 
@@ -34,12 +37,28 @@ function addProducts(products) {
         const productElement = document.createElement('div');
         productElement.classList.add('product');
         productElement.innerHTML = `
-            <img src="${product.image}" alt="${product.title}">
-            <h2>${product.title}</h2>
-            <p>${product.description}</p>
-            <span class="category">${product.category}</span>
-            <span class="color">${product.color}</span>
-            <span class="price">${product.price}</span>
+            <img src="${
+            product.image
+        }" alt="${
+            product.title
+        }">
+            <h2>${
+            product.title
+        }</h2>
+            <p>${
+            product.description
+        }</p>
+            <span data-category="${
+            product.dataCategory
+        }" class="category">${
+            product.category
+        }</span>
+            <span class="color">${
+            product.color
+        }</span>
+            <span class="price">${
+            product.price
+        }</span>
             <button>Acheter</button>
         `;
         main.appendChild(productElement);
@@ -50,6 +69,28 @@ showMoreButton.addEventListener('click', () => {
     addProducts(additionalProducts);
     showMoreButton.style.display = 'none';
 });
+
+const categoryRadioButtons = document.querySelectorAll('input[name="category"]');
+
+categoryRadioButtons.forEach((radio) => {
+    radio.addEventListener('change', filterProducts);
+});
+
+function filterProducts() {
+    const selectedCategory = document.querySelector('input[name="category"]:checked').value;
+
+    const products = document.querySelectorAll('.product');
+
+    products.forEach((product) => {
+        const category = product.querySelector('.category').getAttribute('data-category');
+
+        if ((selectedCategory === 'all' || category === selectedCategory)) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
+}
 
 function sortProducts(sortBy) {
     const products = Array.from(main.querySelectorAll('.product'));
